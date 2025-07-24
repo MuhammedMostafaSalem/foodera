1cd1d18a5bd3de6b2b4ba0d63911ce7cac0a2c33
mohammed <marwansalem164@gmail.com>
2025-07-24 22:25:19 +0300

    Create search component

---
+++ src/components/Sreach.jsx
@@ -0,0 +1,14 @@
+const Sreach = () => {
+    return (
+        <div className="bg-yellow-300 flex justify-between p-[20px] rounded-t-[20px]">
+            <div>
+                <h3>اقسام التطبيق</h3>
+                <p>جدول الخاص بالاقسام</p>
+            </div>
+
+            <input type="search" placeholder="ابحث" className="bg-gray-50 rounded-[10px] px-[10px]" />
+        </div>
+    )
+}
+
+export default Sreach
